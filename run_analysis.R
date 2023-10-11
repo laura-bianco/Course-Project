c30fbfcfa29a36a9962e804607678425e77ea344
laura-bianco <145777244+laura-bianco@users.noreply.github.com>
2023-10-11 13:25:44 +0200

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -4,6 +4,8 @@ features <- read.table("features.txt", stringsAsFactors = FALSE)
 test_data <- read.table("X_test.txt", header = FALSE, sep = "", fill = TRUE)
 activity_test <- read.table("y_test.txt")
 activity_train <- read.table("y_train.txt")
+subject_train <- read.table("subject_train.txt")
+subject_test <- read.table("subject_test.txt")
 train_data <- read.table("X_train.txt", header = FALSE, sep = "", fill = TRUE)
 
 #Renaming them to their features (Step 4 is completed early here)
@@ -11,8 +13,8 @@ colnames(test_data) <- features$V2
 colnames(train_data) <- features$V2
 
 #making two separate data frames
-df_train <- data.frame(train_data, activity_train)
-df_test <- data.frame(test_data, activity_test)
+df_train <- data.frame(train_data, activity_train, subject_train)
+df_test <- data.frame(test_data, activity_test, subject_test)
 
 #merging the two data frames
 merged_df <- rbind(df_train,df_test)
@@ -27,10 +29,11 @@ selected_cols <- merged_df %>%
 activity_labels <- read.table("activity_labels.txt", header = FALSE)
 colnames(selected_cols)[67] <- "Activity"
 selected_cols$Activity <- factor(selected_cols$Activity, levels = activity_labels$V1, labels = activity_labels$V2)
+colnames(selected_cols)[68] <- "Subject"
 
 # Step 5
 tidy_data <- selected_cols %>%
-     group_by(Activity) %>%
+     group_by(Subject, Activity) %>%
      summarise_all(mean)
 
 # writing the tidy data set into a new file
